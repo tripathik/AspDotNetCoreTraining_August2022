814905d4b8c9266de4d7e9be7cbef8be9c31825d
Balkrishna Tripathi <Balkrishna@softura.com>
2022-09-27 12:45:27 +0530

    TASK-3

---
+++ Task-3/Task-3Sln/Task-3/wwwroot/js/apicall.js
@@ -0,0 +1,39 @@
+﻿$(document).ready(
+    function () {
+        $('#submit').click(function () {
+
+
+            $.ajax({
+                url: 'http://localhost:42019/Users/authenticate',
+                dataType: 'json',
+                type: 'post',
+                contentType: 'application/json',
+                data: JSON.stringify({
+                    username: $('#uname').val(),
+                    password: $('#psw').val()
+                }),
+                processData: false,
+                success: function (data, textStatus, jQxhr) {
+                    alert("User successfully authenticated")
+                    if (data.Role == "Manager")
+                    {
+                        window.location.replace('~/Pages//Manager.html')
+                    }
+                    else
+                    {
+                        window.location.replace('~/Pages//Wfm_Manager.html')
+                    }
+                   
+
+                },
+                error: function (jqXhr, textStatus, errorThrown) {
+                    alert("User Failed to authenticate")   
+                }
+            });
+
+
+
+        })
+
+    }
+)
